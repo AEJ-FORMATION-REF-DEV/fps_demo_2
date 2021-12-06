0186c3204c720c7cdffaf92e152aea19b30b5883
kodak10 <55301545+kodak10@users.noreply.github.com>
2021-12-06 08:26:25 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,57 @@
+//load
+$(window).on('load', function() {
+    setTimeout(function(){ 
+        $('.preloader').addClass('preloader-deactivate');
+    }, 3000);
+    
+});
+//carousel
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        nav:true,
+        autoplay:true,
+        autoplayHover:false,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:3
+          }
+      }
+    })
+    });
+
+
+//vers le haut
+    jQuery(document).ready(function($) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
+    });
+
+    $(document).ready(function() {
+        $(window).scroll(function() {
+        if ($(this).scrollTop() > 20) {
+        $('#toTopBtn').fadeIn();
+        } else {
+        $('#toTopBtn').fadeOut();
+        }
+        });
+        
+        $('#toTopBtn').click(function() {
+        $("html, body").animate({
+        scrollTop: 0
+        }, 1000);
+        return false;
+        });
+        });
+
+//modal
