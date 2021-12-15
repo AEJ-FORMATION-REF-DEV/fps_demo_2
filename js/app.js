3affb3c0decbbe7d568882299e2b691a69bf96c9
kodak10 <atchinaymard10@gmail.com>
2021-12-15 13:29:02 +0100

    top

--- js/app.js
+++ js/app.js
@@ -6,6 +6,16 @@ $(window).on('load', function() {
     
 });
 
+//top
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
 
 //carousel
 $(document).ready(function(){
